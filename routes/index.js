c49efe99c988150d471a096dc58ef4e02e98e162
WaffleSquirrel <rockabilly@outlook.com>
2015-11-02 14:39:52 -0800

    Making nappy tings.

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  var items = [];
+  //for(var i = 0; i < 1000; i++) {
+  //  items.push({ nick: 'nick' + i, date: new Date(), args: ['zero', 'one'] });
+  //}
+  res.render('index', {
+      channel: 'Nappy'
+    , buffer: items
+  });
+};
